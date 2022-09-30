0caf795802ab5171fadaa59ae50dc9e62a02d5bc
Kirill <konliri@gmail.com>
2022-09-30 14:56:28 +0800

    Добавлена Задача1

---
+++ Семинар1/Задача1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int sqr = number * number;
+int sqr2 = (int)Math.Pow(number, 3);
+
+Console.Write("Квадрат введённого числа = ");
+Console.WriteLine(sqr);
+Console.Write("Возведение в выбранную степень = ");
+Console.WriteLine(sqr2);
