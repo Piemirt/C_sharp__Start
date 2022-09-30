be1a7e34b0ce84f4c03023f2321b757a527e3bd8
Kirill <konliri@gmail.com>
2022-09-30 17:15:05 +0800

    Решенаа Задача3 Неделя и добавлена Задача5 от -N до N

--- Семинар1/Задача3_Неделя/Program.cs
+++ Семинар1/Задача3_Неделя/Program.cs
@@ -1,2 +1,39 @@
 ﻿// Название дня недели по заданному номеру.
 
+Console.Clear();
+
+Console.Write("Введите номер дня дня недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if(day == 1)
+{
+    Console.WriteLine("Это понедельник =)");
+}
+else if(day == 2)
+{
+    Console.WriteLine("Это вторник =)");
+}
+else if(day == 3)
+{
+    Console.WriteLine("Это среда =)");
+}
+else if(day == 4)
+{
+    Console.WriteLine("Это четверг =)");
+}
+else if(day == 5)
+{
+    Console.WriteLine("Это пятница =)");
+}
+else if(day == 6)
+{
+    Console.WriteLine("Это суббота =)");
+}
+else if(day == 7)
+{
+    Console.WriteLine("Это воскресенье =)");
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует =)");
+}
