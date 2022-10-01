b57b0f2c55270ec7a09070651dba711d2d4e1090
Kirill <konliri@gmail.com>
2022-10-01 08:05:14 +0800

    Решена задача5 Последовательность от -N до N

--- Семинар1/Задача5_-N_N/Program.cs
+++ Семинар1/Задача5_-N_N/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = number * (-1); i <= number; i++)
+{
+    Console.Write(i + $" , ");
+}
