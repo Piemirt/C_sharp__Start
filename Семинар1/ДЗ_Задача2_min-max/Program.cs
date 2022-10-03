d4ae875e00d7a5d905f539d8b4bf7fabdd94dbec
Kirill <konliri@gmail.com>
2022-10-03 15:06:21 +0800

    Добавлена и решена Задача2 для ДЗ к семинару1

---
+++ Семинар1/ДЗ_Задача2_min-max/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7 min = 5
+// a = 2 b = 10 -> max = 10 min = 2
+// a = -9 b = -3 -> max = -3 min = -9
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("max = " + num1);
+    Console.WriteLine("min = " + num2);
+}
+else
+{
+    Console.WriteLine("max = " + num2);
+    Console.WriteLine("min = " + num1);
+}
