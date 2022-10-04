e6b61d74d6dbc074611c0143d0f9ede9bfd0304a
Kirill <konliri@gmail.com>
2022-10-04 17:18:54 +0800

    Добавлена и решена задача Example009 Intro Array Method и добавлена задача Example010 Method Array

--- Examples/Example008_IntroMethod/Program.cs
+++ Examples/Example008_IntroMethod/Program.cs
@@ -1,4 +1,6 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿Console.Clear();
+
+int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if(arg2 > result)
@@ -14,14 +16,12 @@ int c1 = 13;
 int a2 = 2;
 int b2 = 21;
 int c2 = 12;
-int a3 = 1;
+int a3 = 100;
 int b3 = 17;
 int c3 = 11;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-
-int max = Max(max1, max2, max3);
+int max = Max(Max(a1, b1, c1),
+              Max(a2, b2, c2),
+              Max(a3, b3, c3));
 
 Console.WriteLine(max);
