31a5ecb3258b3441504efc8d265068562ea5531a
Kirill <konliri@gmail.com>
2022-10-05 12:52:37 +0800

    Добавлена и решена задача11 Удаление второй цифры случайного трёхзначного числа

---
+++ Семинар2/Задача11_УдалВторЦифры/Program.cs
@@ -0,0 +1,10 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+Console.Clear();
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int hundreds = number / 100;
+int ones = number % 10;
+Console.WriteLine($"{hundreds}{ones}");
