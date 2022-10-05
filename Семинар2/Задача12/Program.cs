0fbd4c1a8d52c8ddb04b3c5160b006aef8895319
Kirill <konliri@gmail.com>
2022-10-06 07:28:52 +0800

    Решена Задача 12 и добавлена Задача14

--- Семинар2/Задача12/Program.cs
+++ Семинар2/Задача12/Program.cs
@@ -1,2 +1,18 @@
-﻿Console.Clear();
+﻿// Задача12. Напишите программу, которая принимает на вход 2 числа и проверяет, кратно ли второе число первому? 
+// Если не кратно, то вывести остаток от деления.
 
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 % number2 == 0)
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+    Console.WriteLine($"Число не кратно, остаток = {number1 % number2}");
+}
