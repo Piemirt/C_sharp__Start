478080a12b33405718a1ccdf52a57de9b9985e29
Kirill <konliri@gmail.com>
2022-10-06 12:45:07 +0800

    Решена Задача16

--- Семинар2/Задача16/Program.cs
+++ Семинар2/Задача16/Program.cs
@@ -6,3 +6,8 @@ Console.Write("Введите первое число: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
+
+if (num1 == num2 * num2 || num2 == num1 * num1)
+    Console.WriteLine("Является квадратом");
+else
+    Console.WriteLine("НЕ является квадратом");
