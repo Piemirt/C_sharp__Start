59405d31bc405a13a3bb90da12a4f3e9a4f0b513
Kirill <konliri@gmail.com>
2022-10-08 08:00:37 +0800

    Добавлена Задача 18 и подправлено решение Задача17 с помощью методов

--- Семинар3/Задача17/Program.cs
+++ Семинар3/Задача17/Program.cs
@@ -1,31 +1,38 @@
 ﻿//  Задача17. Напишите программу, которая принимает на вход координаты 
 //  точки (X и Y), причём X != 0 и Y != 0, и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-Console.Clear();
+void Task17()
+{
+    Console.Clear();
 
-Random random = new Random();
-int x = random.Next(-10, 11);
-int y = random.Next(-10, 11);
+    Random random = new Random();
+    int x = random.Next(-10, 11);
+    int y = random.Next(-10, 11);
+    Console.WriteLine($"A({x}, {y})");
+    CoordCheck(x,y);
+}
 
-Console.WriteLine($"A({x}, {y})");
-
-if (x > 0 && y > 0)
+void CoordCheck(int x, int y)
 {
-    Console.WriteLine("Точка находится в 1 четверти");
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("Точка находится в 1 четверти");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("Точка находится во 2 четверти");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("Точка находится в 3 четверти");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("Точка находится в 4 четверти");
+    }
+    else
+    {
+        Console.WriteLine("Точка находится на одной из осей");
+    }
 }
-else if (x < 0 && y > 0)
-{
-    Console.WriteLine("Точка находится во 2 четверти");
-}
-else if (x < 0 && y < 0)
-{
-    Console.WriteLine("Точка находится в 3 четверти");
-}
-else if (x > 0 && y < 0)
-{
-    Console.WriteLine("Точка находится в 4 четверти");
-}
-else
-{
-    Console.WriteLine("Точка находится на одной из осей");
-}
+Task17();
