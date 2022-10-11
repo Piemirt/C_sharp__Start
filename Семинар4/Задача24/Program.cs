9007bca453c335f5b1f9fd9b8ce373b8081fd9bc
Kirill <konliri@gmail.com>
2022-10-11 19:34:56 +0800

    Решена Задача 24 к Семинару 4

--- Семинар4/Задача24/Program.cs
+++ Семинар4/Задача24/Program.cs
@@ -7,4 +7,13 @@ Console.Clear();
 
 Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine()!);
+int sum = 0;
+Console.WriteLine($"Сумма чисел от 1 до {a}: ");
 
+for (int i = 1; i <= a; i++)
+{
+    Console.Write($"{i} + ");
+    sum += i;
+}
+Console.WriteLine();
+Console.WriteLine($"= {sum}");
