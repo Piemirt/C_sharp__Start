3c8f61a2ef9154377966126386a383b77295c202
Kirill <konliri@gmail.com>
2022-10-12 11:32:44 +0800

    Решена Задача 28 к Семинару 4 и добавлена Задача 30

---
+++ Семинар4/Задача28/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int prod = 1;
+Console.Write($"Произведение чисел от 1 до {n} = ");
+
+for (int i = 1; i <= n; i++)
+{
+    prod *= i;
+}
+Console.Write(prod);
