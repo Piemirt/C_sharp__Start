96d946cbe44b3d6c02b981a109773767ab345bf2
Kirill <konliri@gmail.com>
2022-10-12 12:33:50 +0800

    Решена Задача 30 к Семинару 4

--- Семинар4/Задача30/Program.cs
+++ Семинар4/Задача30/Program.cs
@@ -2,3 +2,46 @@
 
 Console.Clear();
 
+Random random = new Random();
+int size = random.Next(4, 10);
+int[] numbers = new int[size];
+Console.WriteLine(size);
+
+// Заполнение массива с клавиатуры:
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Введите {i + 1}-й элемент массива: ");
+    numbers[i] = int.Parse(Console.ReadLine()!);
+}
+
+//FillArray(numbers);
+// for (int i = 0; i < size; i++)
+// {
+//     numbers[i] = random.Next(0, 2);
+// }
+
+PrintArray(numbers);
+// for (int i = 0; i < size; i++)
+// {
+//     Console.Write(numbers[i] + "  ");
+// }
+// Console.WriteLine();
+
+
+void FillArray(int[] num)
+{
+    Random random = new Random();
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = random.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + "  ");
+    }
+    Console.WriteLine();
+}
