05c2ac05d85e919317268e1e9aeb0176f63fa850
Kirill <konliri@gmail.com>
2022-10-13 11:02:34 +0800

    Решены Задача 32 и Задача 33 к Семинару 4

--- Семинар4/Задача32/Program.cs
+++ Семинар4/Задача32/Program.cs
@@ -2,3 +2,38 @@
 
 Console.Clear();
 
+int[] numbers = CreateArray();
+PrintArray(numbers);
+
+CreateArray();
+RevertArray(numbers);
+PrintArray(numbers);
+
+int[] CreateArray()
+{
+    int[] numbers = { 1, 3, -7, 5, -4 };
+    return numbers;
+}
+
+void RevertArray(int[] numbers)
+{
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = -numbers[i];
+}
+}
+
+void PrintArray(int[] numbers)
+{
+    int count = numbers.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(numbers[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
--- Семинар4/Задача33/Program.cs
+++ Семинар4/Задача33/Program.cs
@@ -1,4 +1,50 @@
 ﻿// Задача 33. Задайте массив. Задайте число. Напишите программу, ктороая определяет, присутствует ли заданное число в массиве.
-// 4; массив [6, 7, 19, 345, 3] --> нет             -3; массив [6, 7, 19, 345, 3] --> да
+// 4; массив [6, 7, 19, 345, -3] --> нет             -3; массив [6, 7, 19, 345, -3] --> да
 
 Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[10];
+
+FillArray(numbers);
+if (FindNumber(numbers, num))
+    Console.WriteLine($"Число {num} присутствует в массиве");
+else
+    Console.WriteLine($"Числа {num} нет в массиве");
+
+PrintArray(numbers);
+
+bool FindNumber(int[] numbers, int num)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+void FillArray(int[] numbers)
+{
+    Random random = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(0, 10);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    int count = numbers.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(numbers[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
