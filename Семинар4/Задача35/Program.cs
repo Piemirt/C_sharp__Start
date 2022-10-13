651dc43abd997f6bcf890640cb7e2a1129df7f58
Kirill <konliri@gmail.com>
2022-10-13 14:56:33 +0800

    Решена Задача 35 к Семинару 4

--- Семинар4/Задача35/Program.cs
+++ Семинар4/Задача35/Program.cs
@@ -3,10 +3,38 @@
 Console.Clear();
 
 int[] array = new int[123];
-
-
-
+FillArray(array);
+PrintArray(array);
+int c = GetCount(array);
 Console.WriteLine($"Количество элементов от 10 до 99 равно {c} ");
+
+int GetCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
 
 void FillArray(int[] array)
 {
