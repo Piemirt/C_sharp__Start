9de88c0a3cc7795511e74eb667ab979cd65e2455
Kirill <konliri@gmail.com>
2022-10-13 16:15:11 +0800

    Решена Задача 37 к Семинару 4

--- Семинар4/Задача37/Program.cs
+++ Семинар4/Задача37/Program.cs
@@ -7,3 +7,39 @@ Console.Clear();
 int[] array = new int[10];
 int[] array2 = new int[5];
 
+FillArray(array);
+PrintArray(array);
+Product(array, array2);
+PrintArray(array2);
+
+void Product(int[] arr, int[] arr2)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        arr2[i] = arr[i] * arr[arr.Length - 1 - i];
+    }
+}
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 20);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
