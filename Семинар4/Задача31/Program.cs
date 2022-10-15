837e1a5f1bcf8765974769eda1e1ae0790b50144
Kirill <konliri@gmail.com>
2022-10-15 12:25:32 +0800

    Добавлено решение Задача 31 к Семинару 4

---
+++ Семинар4/Задача31/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача31. Задайте массив из 12 эл-ов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отриц. и полож. эл-ов массива.
+
+Console.Clear();
+
+int size = 12;
+int[] numbers = new int[size];
+
+FillArray(numbers, -9, 9);
+PrintArray(numbers);
+Console.WriteLine(GetSumPositive(numbers));
+Console.WriteLine(GetSumNegative(numbers));
+
+int GetSumPositive(int[] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+            sum += numbers[i];
+    }
+    return sum;
+}
+
+int GetSumNegative(int[] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] < 0)
+            sum += numbers[i];
+    }
+    return sum;
+}
+
+void FillArray(int[] numbers,
+                int minValue = -50,
+                int maxValue = 50)
+{
+    Random random = new Random();
+    maxValue++;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(minValue, maxValue);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.WriteLine("Вывод массива: ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + "  ");
+    }
+    Console.WriteLine();
+}
