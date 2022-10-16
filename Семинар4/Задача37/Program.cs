0e576bb4daf6da51cb04f2eff32bc0cfc0a5927a
Kirill <konliri@gmail.com>
2022-10-17 07:20:10 +0800

    Добавлено решение Задача 37 к Семинару 4

--- Семинар4/Задача37/Program.cs
+++ Семинар4/Задача37/Program.cs
@@ -2,58 +2,108 @@
 //Результат запишите в новом массиве.
 // [1 2 3 4 5] --> 5 8 3                [6 7 3 6] --> 36 21
 
+// Решение с Мироном --->>>
+
+// Console.Clear();
+
+// int[] array = new int[11];
+
+// FillArray(array);
+// PrintArray(array);
+// int[] array2 = Product(array);
+// PrintArray(array2);
+
+// int[] Product(int[] arr)
+// {
+//     int[] arr2;
+//     if (arr.Length % 2 == 0)
+//     {
+//         arr2 = new int[arr.Length / 2];
+//         for (int i = 0; i < arr.Length / 2; i++)
+//         {
+//             arr2[i] = arr[i] * arr[arr.Length - 1 - i];
+//         }
+//     }
+//     else
+//     {
+//         arr2 = new int[arr.Length / 2 + 1];
+//         for (int i = 0; i < arr.Length / 2 + 1; i++)
+//         {
+//             if (i == arr.Length / 2)
+//                 arr2[i] = arr[i];
+//             else
+//                 arr2[i] = arr[i] * arr[arr.Length - 1 - i];
+//         }
+//     }
+//     return arr2;
+// }
+
+// void FillArray(int[] array)
+// {
+//     Random random = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = random.Next(0, 20);
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     Console.Write("[");
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write(array[i]);
+//         if (i == count - 1)
+//             Console.WriteLine("]");
+//         else
+//             Console.Write(", ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Решение с Денисом Сапрыкиным --->>>
+
 Console.Clear();
 
-int[] array = new int[11];
+int size = 7;
+int[] numbers = new int[size];
 
-FillArray(array);
-PrintArray(array);
-int[] array2 = Product(array);
-PrintArray(array2);
+FillArray(numbers);
+PrintArray(numbers);
+SumPairs(numbers);
 
-int[] Product(int[] arr)
+void SumPairs(int[] numbers)
 {
-    int[] arr2;
-    if (arr.Length % 2 == 0)
+    int maxIndex = size - 1;
+    for (int i = 0; i < size / 2; i++)
     {
-        arr2 = new int[arr.Length / 2];
-        for (int i = 0; i < arr.Length / 2; i++)
-        {
-            arr2[i] = arr[i] * arr[arr.Length - 1 - i];
-        }
+        Console.WriteLine($"{numbers[i]} * {numbers[maxIndex - i]} = {numbers[i] * numbers[maxIndex - i]}");
     }
-    else
-    {
-        arr2 = new int[arr.Length / 2 + 1];
-        for (int i = 0; i < arr.Length / 2 + 1; i++)
-        {
-            if (i == arr.Length / 2)
-                arr2[i] = arr[i];
-            else
-                arr2[i] = arr[i] * arr[arr.Length - 1 - i];
-        }
-    }
-    return arr2;
+    if (size % 2 == 1)
+        Console.WriteLine("Средний элемент массива = " + numbers[size / 2]);
+    Console.WriteLine();
 }
 
-void FillArray(int[] array)
+void FillArray(int[] numbers)
 {
     Random random = new Random();
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < numbers.Length; i++)
     {
-        array[i] = random.Next(0, 20);
+        numbers[i] = random.Next(-10, 11);
     }
 }
 
-void PrintArray(int[] array)
+void PrintArray(int[] numbers)
 {
-    int count = array.Length;
+    int count = numbers.Length;
     Console.Write("[");
     for (int i = 0; i < count; i++)
     {
-        Console.Write(array[i]);
+        Console.Write(numbers[i]);
         if (i == count - 1)
-            Console.WriteLine("]");
+            Console.Write("]");
         else
             Console.Write(", ");
     }
