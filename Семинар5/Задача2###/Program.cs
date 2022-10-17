ad640c7c0f7d0d34ff2bd3ce0f06887690db37b6
Kirill <konliri@gmail.com>
2022-10-17 16:35:36 +0800

    Решена Задача 2###(доп) к Семинару 5

--- Семинар5/Задача2###/Program.cs
+++ Семинар5/Задача2###/Program.cs
@@ -3,3 +3,35 @@
 
 Console.Clear();
 
+Random random = new Random();
+int countOnes = random.Next(1, 11);
+Console.WriteLine("Количество единиц: " + countOnes);
+int size = countOnes * countOnes;
+int[] numbers = new int[size];
+int randomIndex = random.Next(0, size);
+for (int i = 0; i < countOnes; i++)
+{
+    while (numbers[randomIndex] == 1)
+    {
+        Console.WriteLine($"Элемент {randomIndex + 1} уже равен 1, ищем следующий");
+        randomIndex = random.Next(0, size);
+    }
+    numbers[randomIndex] = 1;
+    Console.WriteLine($"Элемент {randomIndex + 1} стал 1");
+}
+PrintArray(numbers);
+
+void PrintArray(int[] numbers)
+{
+    int count = numbers.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(numbers[i]);
+        if (i == count - 1)
+            Console.Write("]");
+        else
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
