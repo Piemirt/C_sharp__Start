7250208fda2f2705a1f211c088f89041c909c028
Kirill <konliri@gmail.com>
2022-10-17 12:43:08 +0800

    Решена Задача 29 к ДЗ к Семинару 4

--- Семинар4/ДЗ_Задача29/Program.cs
+++ Семинар4/ДЗ_Задача29/Program.cs
@@ -4,3 +4,41 @@
 //                      Ограничения:
 // - Должна быть отдельная функция, которая создаёт массив и отдельная функция вывода
 // - Внутри функций (кроме функции вывода) НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода или в функции вывода
+
+Console.Clear();
+
+int size = 8;
+int[] numbers = CreateArray();
+
+FillArray(numbers);
+PrintArray(numbers);
+
+int[] CreateArray()
+{
+    int[] numbers = new int[size];
+    return numbers;
+}
+
+void FillArray(int[] numbers)
+{
+    Random random = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next();
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    int count = numbers.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(numbers[i]);
+        if (i == count - 1)
+            Console.Write("]");
+        else
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
