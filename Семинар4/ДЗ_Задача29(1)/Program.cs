3dfd28b66fa5180c05025c38ce3e13774c04cf51
Kirill <konliri@gmail.com>
2022-10-17 14:49:29 +0800

    Решена Задача 29(1) к Семинару 4

---
+++ Семинар4/ДЗ_Задача29(1)/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив.
+
+Console.Clear();
+
+int size = 10;
+int[] numbers = new int[size];
+
+FillArray(numbers);
+Console.WriteLine("Вывод первоначального массива: ");
+PrintArray(numbers);
+
+for (int i = 1; i < size; i++)
+{
+    for (int j = 0; j < size - i; j++)
+    {
+        if (Math.Abs(numbers[j]) > Math.Abs(numbers[j + 1]))
+        {
+            int temp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+        }
+    }
+}
+Console.WriteLine("Вывод отсортированного по модулю массива: ");
+PrintArray(numbers);
+
+void FillArray(int[] numbers)
+{
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = random.Next(-30, 30);
+    }
+}
+
+void PrintArray( int[] numbers)
+{
+    int count = numbers.Length;
+    Console.Write("[ ");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(numbers[i]);
+        if (i == count -1)
+            Console.Write(" ] ");
+        else
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
