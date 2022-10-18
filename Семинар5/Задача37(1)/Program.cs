d064277bc88e4dafb74fd265f43a83ba2cc7102b
Kirill <konliri@gmail.com>
2022-10-18 15:54:52 +0800

    Решена Задача 37(1) к Семинару 5

--- Семинар5/Задача37(1)/Program.cs
+++ Семинар5/Задача37(1)/Program.cs
@@ -59,3 +59,32 @@ void PrintArray(int[] array)
     }
     Console.Write(" ]");
 }
+
+
+// Решение №2  --->>>
+/*
+Console.Clear();
+
+Console.Write("Введите числа первого массива через пробел: ");
+string firstArray = Console.ReadLine()!;
+
+Console.Write("Введите числа второго массива через пробел: ");
+string secondArray = Console.ReadLine()!;
+
+int[] newArray = ParseToArray(firstArray, secondArray);
+
+Console.WriteLine(String.Join(", ", newArray));
+
+int[] ParseToArray(string str1, string str2)
+{
+    string str = str1 + ' ' + str2;
+    string[] stringArray = str.Split(" ");
+    int[] result = new int[stringArray.Length];
+
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = int.Parse(stringArray[i]);
+    }
+    return result;
+}
+*/
