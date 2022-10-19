ce360afa7455e0bce3410e1fde4ed83a44da5898
Kirill <konliri@gmail.com>
2022-10-20 07:30:33 +0800

    Решена Задача 34 к ДЗ для Семинара 5

--- Семинар5/ДЗ_Задача34/Program.cs
+++ Семинар5/ДЗ_Задача34/Program.cs
@@ -1,5 +1,42 @@
 ﻿// Задача 34: Задайте массив, заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
+// Ограничения:
+// Внутри функций (кроме функции вывода) НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода или в функции вывода
 
 Console.Clear();
 
+int size = 8;
+int[] numbers = new int[size];
+
+FillArray(numbers, 100, 1000);
+PrintArray(numbers);
+
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    if (numbers[i] % 2 == 0)
+        count++;
+}
+Console.WriteLine("Количество чётных чисел = " + count);
+
+void FillArray(int[] numbers, int minValue = 10, int maxValue = 100)
+{
+    Random random = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(minValue, maxValue);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    int length = numbers.Length;
+    Console.Write("[ ");
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(numbers[i]);
+        if (i < length - 1)
+            Console.Write(", ");
+    }
+    Console.WriteLine(" ]");
+}
