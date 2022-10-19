54e7e24a9b2cc8b20c84041c7032060481e0c253
Kirill <konliri@gmail.com>
2022-10-19 12:25:39 +0800

    Решена Задача 35(1) и Задача 39 к Семинару 5

--- Семинар5/Задача35(1)/Program.cs
+++ Семинар5/Задача35(1)/Program.cs
@@ -3,3 +3,41 @@
 
 Console.Clear();
 
+Console.Write("Введите числа первого массива через пробел: ");
+string firstArray = Console.ReadLine()!;
+
+Console.Write("Введите числа второго массива через пробел: ");
+string secondArray = Console.ReadLine()!;
+
+int[] fArray = ParseToArray(firstArray);
+int[] sArray = ParseToArray(secondArray);
+
+if (fArray.Length != sArray.Length)
+    Console.WriteLine("Длины массивов не равны! ");
+else
+{
+    int[] res = SumOfArray(fArray, sArray);
+    Console.Write(String.Join(", ", res));
+}
+
+int[] ParseToArray(string str)
+{
+    string[] stringArray = str.Split(" ");
+    int[] result = new int[stringArray.Length];
+
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = int.Parse(stringArray[i]);
+    }
+    return result;
+}
+
+int[] SumOfArray(int[] firstArray, int[] secondArray)
+{
+    int[] result = new int[firstArray.Length];
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        result[i] = firstArray[i] + secondArray[i];
+    }
+    return result;
+}
---
+++ Семинар5/Задача39/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 39. Напишите программу, которая перевернёт одномерный массив (последний эл. будет на первом месте, а первый - на последнем и т.д.)
+
+Console.Clear();
+
+Console.Write("Введите элементы массива через пробел: ");
+string array = Console.ReadLine()!;
+
+int[] newArray = ParseToArray(array);
+int[] newNewArray = Reverse(newArray);
+
+Console.Write(String.Join(", ", newNewArray));
+
+int[] ParseToArray(string str)
+{
+    string[] stringArray = str.Split(" ");
+    int[] result = new int[stringArray.Length];
+
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = int.Parse(stringArray[i]);
+    }
+    return result;
+}
+
+int[] Reverse(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int c = array[i];
+        array[i] = array[array.Length - i -1];
+        array[array.Length - i -1] = c;
+    }
+    return array;
+}
