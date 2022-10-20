4d313c155d7167118d6cf3998202d77d7909f5fe
Kirill <konliri@gmail.com>
2022-10-20 21:19:41 +0800

    Решена Задача 44 к Семинару 6

--- Семинар6/Задача44/Program.cs
+++ Семинар6/Задача44/Program.cs
@@ -1,3 +1,29 @@
 ﻿//Задача 44. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Ф.: 0 и 1.
 
 Console.Clear();
+
+Console.Write("Введите количество для расчёта чисел Фибоначчи: ");
+int size = int.Parse(Console.ReadLine()!);
+int a = 0;
+int b = 1;
+double[] numbers = new double[size];
+numbers[0] = a;
+numbers[1] = b;
+for (int i = 2; i < numbers.Length; i++)
+{
+    numbers[i] = numbers[i - 1] + numbers[i - 2];
+}
+PrintArray(numbers);
+
+void PrintArray(double[] numbers)
+{
+    int length = numbers.Length;
+    Console.Write("[ ");
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(numbers[i]);
+        if (i < length - 1)
+            Console.Write(", ");
+    }
+    Console.WriteLine(" ]");
+}
