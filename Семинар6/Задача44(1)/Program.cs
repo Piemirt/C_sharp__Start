da81eed2188dd9f31b49a94ebc75217b69d837e7
Kirill <konliri@gmail.com>
2022-10-21 17:47:55 +0800

    Решена Задача 44(1) к Семинару 6

--- Семинар6/Задача44(1)/Program.cs
+++ Семинар6/Задача44(1)/Program.cs
@@ -4,3 +4,39 @@
 
 Console.Clear();
 
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int coloumns = int.Parse(Console.ReadLine()!);
+int[,] table = CreateArray(rows, coloumns);
+FillArray(table);
+PrintArray(table);
+
+int[,] CreateArray(int rows, int coloumns)
+{
+    int[,] table = new int[rows, coloumns];
+    return table;
+}
+
+void FillArray(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().Next(0, 2);
+        }
+    }
+}
+
+void PrintArray(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($" | {table[i,j]} | ");
+        }
+        Console.WriteLine();
+    }
+}
