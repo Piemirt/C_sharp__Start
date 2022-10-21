2e989481023ca327a85e53b739430733144978a8
Kirill <konliri@gmail.com>
2022-10-21 17:12:38 +0800

    Решены Задачи 39(2) и 39(3) к Семинару 6

--- Семинар6/Задача39(2)/Program.cs
+++ Семинар6/Задача39(2)/Program.cs
@@ -3,3 +3,27 @@
 
 Console.Clear();
 
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"В числе {num} количество цифр = {CountDigits(num)}");
+
+//Вариант со счётчиком: --->>>
+/*
+int CountDigits(int num, int counter = 0)
+{
+    if (num / 10 == 0)
+        return counter + 1;
+    else
+        return counter + CountDigits(num / 10) + 1;
+}
+*/
+
+//Вариант без счётчика: --->>>
+
+int CountDigits(int num)
+{
+    if (num / 10 < 1)
+        return 1;
+    else
+        return 1 + CountDigits(num / 10);
+}
--- Семинар6/Задача39(3)/Program.cs
+++ Семинар6/Задача39(3)/Program.cs
@@ -3,3 +3,41 @@
 
 Console.Clear();
 
+int size = 5;
+int[] numbers = new int[size];
+FillArray(numbers);
+PrintArray(numbers);
+Console.WriteLine();
+Console.Write("Рекурсивный массив: ");
+RecurPrintArray(numbers);
+
+void PrintArray(int[] numbers)
+{
+    int length = numbers.Length;
+    Console.Write("Массив: ");
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+}
+
+void RecurPrintArray(int[] numbers, int i = 0)
+{
+    int length = numbers.Length;
+    if (i < length)
+    {
+        Console.Write(numbers[i] + " ");
+        i++;
+        RecurPrintArray(numbers, i);
+    }
+}
+
+void FillArray(int[] numbers)
+{
+    int length = numbers.Length;
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        numbers[i] = random.Next(-50, 50);
+    }
+}
