4623bf2ec9284aaf7567bc665fb8c504f977c7b0
Kirill <konliri@gmail.com>
2022-10-22 11:57:16 +0800

    Решена Задача со звёздочкой к Семинару 5

--- Семинар5/ДЗ_Задача_со_звёздочкой/Program.cs
+++ Семинар5/ДЗ_Задача_со_звёздочкой/Program.cs
@@ -1,8 +1,48 @@
-﻿// Задача со звёздочкой:
-// Найти в массиве самую длинную последовательность, состоящую из одинаковых элементов. 
-// Вывести на экран количество элементов самой длинной последовательности, 
-// элемент самой последовательности и номер элемента, который является её началом.
+﻿// Задача со звёздочкой: Найти в массиве самую длинную последовательность, состоящую из одинаковых элементов. 
+// Вывести на экран кол-во эл-ов самой длинной последовательности, эл-т самой последовательности и номер эл-та, который является её началом.
 // [1 2 2 4 4 4 4 3 3 3] -> Count: 4     Element: 4     Position: 3
 
 Console.Clear();
 
+int[] numbers = CreateArray();
+int[] result = FindMaxSequence(numbers);
+Console.WriteLine($"Element: {result[0]}");
+Console.WriteLine($"Count: {result[1]}");
+Console.WriteLine($"Start: {result[2]}");
+
+int[] CreateArray()
+{
+    return new int[] { 1, 2, 2, 4, 4, 4, 4, 3, 3, 3, 4 };
+}
+
+int[] FindMaxSequence(int[] numbers)
+{
+    int[] result = new int[3];
+    int currentSequenceElement = numbers[0];
+    int currentSequenceCounter = 0;
+    int currentSequenceStart = 0;
+
+    int maxSequenceElement = numbers[0];
+    int maxSequenceCounter = 0;
+    int maxSequenceStart = 0;
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] != currentSequenceElement)
+        {
+            // сравнить текущую последовательность с максимальной
+            if (currentSequenceCounter > maxSequenceCounter)
+            {
+                maxSequenceElement = currentSequenceElement;
+                maxSequenceCounter = currentSequenceCounter;
+                maxSequenceStart = currentSequenceStart;
+            }
+
+            currentSequenceElement = numbers[i];
+            currentSequenceCounter = 0;
+            currentSequenceStart = i;
+        }
+        currentSequenceCounter++;
+    }
+    return new int[] { maxSequenceElement, maxSequenceCounter, maxSequenceStart };
+}
