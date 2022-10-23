b88111d0562bc9f81dc46de504221a05e227fa59
Kirill <konliri@gmail.com>
2022-10-23 16:13:48 +0800

    Добавлена Задача 46 к Семинару 7

---
+++ Семинар7/Задача46/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 46. Задайте двумерный массив размером m*n, заполненный случайными целыми числами.
+
+Console.Clear();
+
+Random random = new Random();
+int rows = random.Next(4,8);
+int colomns = random.Next(4,8);
+
