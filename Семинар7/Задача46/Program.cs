730c293472a2000ed08041a7543179ee1e4daa53
Kirill <konliri@gmail.com>
2022-10-24 07:39:07 +0800

    Решена Задача 46 и добавлена Задача 48 к Семинару 7

--- Семинар7/Задача46/Program.cs
+++ Семинар7/Задача46/Program.cs
@@ -3,6 +3,37 @@
 Console.Clear();
 
 Random random = new Random();
-int rows = random.Next(4,8);
-int colomns = random.Next(4,8);
+int rows = random.Next(4, 8);
+int columns = random.Next(4, 8);
+Console.WriteLine($"Массив размером {rows} x {columns}: ");
+int[,] numbers = new int[rows, columns];
+FillArray(numbers);
+PrintArray(numbers);
 
+void FillArray(int[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(-10, 11);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
