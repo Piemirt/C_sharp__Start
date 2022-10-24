cf2858e9cc2f058246c0a2b459140b9529f57464
Kirill <konliri@gmail.com>
2022-10-24 15:49:54 +0800

    Решены Задача 49 и Задача 51 и добавлена Задача 51(доп мячи) к Семинару 7

--- Семинар7/Задача49/Program.cs
+++ Семинар7/Задача49/Program.cs
@@ -2,3 +2,48 @@
 
 Console.Clear();
 
+Random random = new Random();
+int rows = random.Next(4, 8);
+int columns = random.Next(4, 8);
+Console.WriteLine($"Массив размером {rows} * {columns}: ");
+int[,] numbers = new int[rows, columns];
+FillArray(numbers);
+PrintArray(numbers);
+
+for (int i = 0; i < rows; i += 2)
+{
+    for (int j = 0; j < columns; j += 2)
+    {
+        numbers[i, j] = numbers[i, j] * numbers[i, j];
+    }
+}
+Console.WriteLine();
+PrintArray(numbers);
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(0, 11);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
--- Семинар7/Задача51/Program.cs
+++ Семинар7/Задача51/Program.cs
@@ -2,3 +2,48 @@
 
 Console.Clear();
 
+Random random = new Random();
+int rows = random.Next(4, 8);
+int columns = rows;
+int[,] numbers = new int[rows, columns];
+int sum = 0;
+Console.WriteLine($"Массив размером {rows} * {columns}: ");
+FillArray(numbers);
+PrintArray(numbers);
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        if (i == j)
+            sum = sum + numbers[i, j];
+    }
+}
+Console.WriteLine($"Сумма элементов по главной диагонали = {sum}");
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(0, 11);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
