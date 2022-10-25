18fc9ea7b1f0f8bef84d2f4fe6c2fd80984ad664
Kirill <konliri@gmail.com>
2022-10-25 18:43:33 +0800

    Решена Задача(доп2) для ДЗ к Семинару 6

--- Семинар6/ДЗ_Задача_доп2/Program.cs
+++ Семинар6/ДЗ_Задача_доп2/Program.cs
@@ -5,3 +5,16 @@
 
 Console.Clear();
 
+double average = CalcAverageFromConsole();
+Console.WriteLine(average);
+
+double CalcAverageFromConsole(double sum = 0, int count = 0)
+{
+    Console.Write("Введите положительное число: ");
+    double number = double.Parse(Console.ReadLine()!);
+
+    if (number == -1)
+        return sum / count;
+    else
+        return CalcAverageFromConsole(sum + number, count + 1);
+}
