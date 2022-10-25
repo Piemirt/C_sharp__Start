33d0d7b1898ce62412d43997ee704591d53fd128
Kirill <konliri@gmail.com>
2022-10-25 11:17:26 +0800

    Решена Задача 46(1) и Добавлена Задача 48(1) к Семинару 7

--- Семинар7/Задача46(1)/Program.cs
+++ Семинар7/Задача46(1)/Program.cs
@@ -3,3 +3,45 @@
 
 Console.Clear();
 
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите z: ");
+int z = int.Parse(Console.ReadLine()!);
+
+int[,,] numbers = new int[m, n, z];
+FillArray(numbers);
+PrintArray(numbers);
+
+
+void PrintArray(int[,,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            for (int k = 0; k < numbers.GetLength(2); k++)
+            {
+                Console.Write($"{numbers[i, j, k]} ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,,] numbers)
+{
+    Random random = new Random();
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            for (int k = 0; k < numbers.GetLength(2); k++)
+            {
+                numbers[i, j, k] = random.Next(0, 100);
+            }
+        }
+    }
+}
