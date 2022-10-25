eca1a0d177826d1fb3af88aef56a4714cac3e325
Kirill <konliri@gmail.com>
2022-10-25 11:37:43 +0800

    Решена Задача 48(1) и Добавлены Задача 49(1) и Задача 51(1) к Семинару 7

--- Семинар7/Задача48(1)/Program.cs
+++ Семинар7/Задача48(1)/Program.cs
@@ -3,3 +3,34 @@
 
 Console.Clear();
 
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+int[,] numbers = new int[m, n];
+FillArray(numbers);
+PrintArray(numbers);
+
+
+void PrintArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write($"{numbers[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = i + j;
+        }
+    }
+}
