260ff4d4e8a001ea30f742465b89bfa33b490e50
Kirill <konliri@gmail.com>
2022-10-25 15:01:44 +0800

    Решены Задача 49(1) и Задача 51(1) к Семинару 7 и Добавлены Задача 47, 50, 52 для ДЗ к Семинару 7

--- Семинар7/Задача49(1)/Program.cs
+++ Семинар7/Задача49(1)/Program.cs
@@ -3,3 +3,52 @@
 
 Console.Clear();
 
+Random random = new Random();
+int rows = random.Next(4, 6);
+int columns = random.Next(4, 6);
+int[,] numbers = new int[rows, columns];
+Console.WriteLine($"Массив размером {rows} * {columns}: ");
+FillArray(numbers);
+PrintArray(numbers);
+ReplaceArray(numbers);
+Console.WriteLine();
+PrintArray(numbers);
+
+void ReplaceArray(int[,] numbers)
+{
+    for (int i = 0; i < rows; i += 2)
+    {
+        for (int j = 0; j < columns; j += 2)
+        {
+            numbers[i, j] = (int)Math.Pow(numbers[i, j], 2);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(1, 6);
+        }
+    }
+}
--- Семинар7/Задача51(1)/Program.cs
+++ Семинар7/Задача51(1)/Program.cs
@@ -3,3 +3,53 @@
 
 Console.Clear();
 
+Random random = new Random();
+int rows = random.Next(5, 6);
+int columns = random.Next(5, 6);
+int[,] numbers = new int[rows, columns];
+Console.WriteLine($"Массив размером {rows} * {columns}: ");
+FillArray(numbers);
+PrintArray(numbers);
+Console.WriteLine($"Сумма элементов главной диагонали = {SumOfMainDiag(numbers)}");
+
+int SumOfMainDiag(int[,] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (i == j)
+                sum = sum + numbers[i, j];
+        }
+    }
+    return sum;
+}
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
