1a912da618687f61d5b825bd8787b4b27492f05e
Kirill <konliri@gmail.com>
2022-10-26 15:15:44 +0800

    Решены Задача 53, 55 и Добавлена Задача 57 к Семинару 8

--- Семинар8/Задача53/Program.cs
+++ Семинар8/Задача53/Program.cs
@@ -3,3 +3,51 @@
 
 Console.Clear();
 
+Random random = new Random();
+int rows = random.Next(4, 7);
+int columns = random.Next(4, 7);
+int[,] numbers = new int[rows, columns];
+Console.WriteLine($"Массив размером: {rows} * {columns} ");
+FillArray(numbers);
+PrintArray(numbers);
+Console.WriteLine("Первая и последняя строки в массиве поменялись местами: ");
+ChangeRows(numbers);
+PrintArray(numbers);
+
+void ChangeRows(int[,] numbers)
+{
+    int minIndex = 0;
+    int maxIndex = rows - 1;
+    for (int j = 0; j < columns; j++)
+    {
+        (numbers[minIndex, j], numbers[maxIndex, j]) = (numbers[maxIndex,j], numbers[minIndex, j]);
+    }
+}
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
--- Семинар8/Задача55/Program.cs
+++ Семинар8/Задача55/Program.cs
@@ -4,3 +4,52 @@
 
 Console.Clear();
 
+Random random = new Random();
+int rows = random.Next(4, 7);
+int columns = rows;
+int[,] numbers = new int[rows, columns];
+Console.WriteLine($"Массив размером: {rows} * {columns} ");
+FillArray(numbers);
+PrintArray(numbers);
+Console.WriteLine("Строки в массиве стали столбцами: ");
+ChangeRowsColumns(numbers);
+PrintArray(numbers);
+
+void ChangeRowsColumns(int[,] numbers)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = i; j < columns; j++)
+        {
+            (numbers[i, j], numbers[j, i]) = (numbers[j, i], numbers[i, j]);
+        }
+    }
+}
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
