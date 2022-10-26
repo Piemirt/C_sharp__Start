f4c25eb51c8f31c95acf19ce218ed122b04bd15f
Kirill <konliri@gmail.com>
2022-10-26 16:53:56 +0800

    Решена Задача 57 к Семинару 8

--- Семинар8/Задача57/Program.cs
+++ Семинар8/Задача57/Program.cs
@@ -4,3 +4,52 @@
 
 Console.Clear();
 
+Random random = new Random();
+int rows = random.Next(4, 8);
+int columns = random.Next(4, 8);
+int[,] numbers = new int[rows, columns];
+Console.WriteLine($"Массив размером: {rows} * {columns} ");
+FillArray(numbers);
+PrintArray(numbers);
+int[] counts = new int[10];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        counts[numbers[i, j]]++;
+    }
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Количество {i} в массиве: {counts[i]}");
+}
+
+void FillArray(int[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
