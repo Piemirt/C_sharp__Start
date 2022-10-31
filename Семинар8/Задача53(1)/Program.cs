a446b1fb1456d41316497555d7d5a5aeb263fb9d
Kirill <konliri@gmail.com>
2022-10-31 15:12:55 +0800

    Добавлены и решены Задачи 53(1) и 55(1) к Семинару 8

---
+++ Семинар8/Задача53(1)/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 53. Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строки массива.
+// Решение с Мироном: --->>>
+
+Console.Clear();
+
+int rows = 5;
+int columns = 5;
+int[,] matrix = new int[rows, columns];
+FillMatrix(matrix, 0, 9);
+PrintMatrix(matrix);
+Console.WriteLine("Первая строка матрицы поменялась с последней строкой: ");
+ChangeMatrixRows(matrix);
+PrintMatrix(matrix);
+
+
+void ChangeMatrixRows(int[,] matrix)
+{
+    int minIndex = 0;
+    int maxIndex = rows - 1;
+    for (int j = 0; j < columns; j++)
+    {
+        (matrix[minIndex, j], matrix[maxIndex, j]) = (matrix[maxIndex, j], matrix[minIndex, j]);
+    }
+}
+
+void FillMatrix(int[,] matrix, int minValue = 0, int maxValue = 0)
+{
+    maxValue++;
+    Random random = new Random();
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = random.Next(minValue, maxValue);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Семинар8/Задача55(1)/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 55. Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+// Решение с Денисом Сапрыкиным: --->>>
+
+Console.Clear();
+
+int rows = 5;
+int columns = rows;
+int[,] matrix = new int[rows, columns];
+FillMatrix(matrix, 0, 9);
+PrintMatrix(matrix);
+Console.WriteLine("Строки стали столбцами и наоборот: ");
+ChangeRowsColumns(matrix);
+PrintMatrix(matrix);
+
+void ChangeRowsColumns(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = i; j < columns; j++)
+        {
+            (matrix[i,j], matrix[j,i]) = (matrix[j,i], matrix[i,j]);
+        }
+    }
+}
+
+void FillMatrix(int[,] matrix, int minValue = 0, int maxValue = 0)
+{
+    Random random = new Random();
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = random.Next(minValue, maxValue);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
