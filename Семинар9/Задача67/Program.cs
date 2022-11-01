5699401004f68671749594baef9b814a75b72b10
Kirill <konliri@gmail.com>
2022-11-01 14:37:56 +0800

    Решена Задача 67 к Семинару 9 и Добавлены Задачи 64, 66 и 68 для ДЗ к Семинару 9

--- Семинар9/Задача67/Program.cs
+++ Семинар9/Задача67/Program.cs
@@ -1,5 +1,17 @@
-﻿// Задача 67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+﻿// Задача 67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. С помощью рекурсии.
 // Решение с Денисом: --->>>
 
 Console.Clear();
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+Console.Write($"Сумма цифр в заданном числе = {GetSumNumbersRecursion(number)}");
+
+int GetSumNumbersRecursion(int number, int sum = 0)
+{
+    if (number == 0)
+        return sum;
+    sum = sum + number % 10;
+    number = number / 10;
+    return GetSumNumbersRecursion(number, sum);
+}
--- Семинар9/Задача69/Program.cs
+++ Семинар9/Задача69/Program.cs
@@ -3,3 +3,27 @@
 
 Console.Clear();
 
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine()!);
+// int result = 1;
+// for (int i = 0; i < b; i++)
+// {
+//     result *= a;
+// }
+// Console.Write($" {a}^{b} = {result}");
+
+Pow(a,b);
+
+void Pow(int a, int b, int i = 0, int result = 1)
+{
+    if (i >= b)
+    {
+        Console.Write($" {a}^{b} = {result}");
+        return;
+    }
+    result *= a;
+    i++;
+    Pow(a, b, i, result);
+}
