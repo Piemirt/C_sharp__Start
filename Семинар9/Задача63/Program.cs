6ae20b96ef227dea813edc526d92179f78044e44
Kirill <konliri@gmail.com>
2022-11-01 11:53:09 +0800

    Решены Задачи 63 и 65 к Семинару 9

--- Семинар9/Задача63/Program.cs
+++ Семинар9/Задача63/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Задача 63. Задайте значене N. Напишите программу, кторая через рекурсию выведет все натуральные числа от 1 до N.
+// Решение с Денисом: --->>>
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+FindNumRecursion(number);
+
+void FindNumRecursion(int number, int counter = 0)
+{
+    counter++;
+    Console.WriteLine(counter);
+    if (counter >= number)
+        return;
+    FindNumRecursion(number, counter);
+}
--- Семинар9/Задача65/Program.cs
+++ Семинар9/Задача65/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натур-ые числа в промежутке от M до N. Рекурсивно.
+// Решение с Денисом: --->>>
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine()!);
+FindNumRecursion(number, numberM);
+
+void FindNumRecursion(int number, int counter = 0)
+{
+    Console.WriteLine(counter);
+    counter++;
+    if (counter > number)
+        return;
+    FindNumRecursion(number, counter);
+}
