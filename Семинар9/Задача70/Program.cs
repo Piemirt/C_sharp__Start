e726312448eb769f2de497407e713dfe722dc33f
Kirill <konliri@gmail.com>
2022-11-02 11:10:13 +0800

    Решены Задачи 70 и 71 и Добавлена Задача 72 к Семинару 9

---
+++ Семинар9/Задача70/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 70: Напишите программу, которая печатает последовательность чисел, где каждое следующее равно сумме двух предыдущих. 
+// На вход принимает 3 числа: n1, n2 и N, где n1 и n2 - первые числа последовательности, N – длина последовательности
+// 3 и 4, N = 5 -> 3 4 7 11 18                  6 и 10, N = 4 -> 6 10 16 26
+// Решение с Мироном: --->>>
+
+Console.Clear();
+/*
+Console.Write("Введите первое число последовательности: ");
+int numOne = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число последовательности: ");
+int numTwo = int.Parse(Console.ReadLine()!);
+Console.Write("Введите длину последовательности: ");
+int numThree = int.Parse(Console.ReadLine()!);
+
+int[] result = RowLikeFibo(numThree, numOne, numTwo);
+Console.Write(string.Join(", ", result));
+
+int[] RowLikeFibo(int digit, int firstNum, int secondNum)
+{
+    int[] numbers = new int[digit];
+    numbers[0] = firstNum;
+    numbers[1] = secondNum;
+
+    for (int i = 2; i < numbers.Length; i++)
+    {
+        numbers[i] = numbers[i - 1] + numbers[i - 2];
+    }
+    return numbers;
+}
+*/
+
+//Ещё решение: --->>>
+
+Console.Write("Введите превое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите длину последовотельности: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write($"{num1} {num2} ");
+for (int i = 1; i <= n - 2; i++)
+{
+    int num3 = num1 + num2;
+    Console.Write($"{num3} ");
+    num1 = num2;
+    num2 = num3;
+}
---
+++ Семинар9/Задача71/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 71: В некотором машинном алфавите имеются четыре буквы: «а», «и», «с» и «в». Покажите все слова, состоящие из 2 букв, 
+// которые можно построить из букв этого алфавита.      n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са, ав, ва, ви, ив, св, вс
+// Решение с Мироном: --->>>
+/*
+Console.Clear();
+
+char[] alphabet = {'а', 'и', 'с', 'в' };
+
+Console.Write("Сколько букв в слове? ");
+int num = int.Parse(Console.ReadLine()!);
+
+char[] word = new char[num];
+
+WriteWord(alphabet, word);
+
+void WriteWord(char[] array, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{new String(word)}");
+        return;
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        word[length] = array[i];
+        WriteWord(array, word, length + 1);
+    }
+}
+*/
+
+// Ещё решение:
+
+Console.Clear();
+
+string[] letters = { "а", "и", "с", "в" };
+
+Console.Write("Сколько букв в слове? ");
+int num = int.Parse(Console.ReadLine()!);
+
+PrintAllWords(letters, num);
+
+void PrintAllWords(string[] alphabet, int length, string prefix = "")
+{
+    if (length == 0)
+    {
+        Console.WriteLine(prefix);
+    }
+    else
+    {
+        for (int i = 0; i < letters.Length; i++)
+        {
+            PrintAllWords(alphabet, length - 1, prefix + letters[i]);
+        }
+    }
+}
