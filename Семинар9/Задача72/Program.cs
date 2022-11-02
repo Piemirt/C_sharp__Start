08c23f069ff4ca5ff3cc4fc26ffa2449e8b8c51f
Kirill <konliri@gmail.com>
2022-11-02 11:42:52 +0800

    Решены Задачи 72 и начало решения Задача 72(доп) к Семинару 9

--- Семинар9/Задача72/Program.cs
+++ Семинар9/Задача72/Program.cs
@@ -4,3 +4,35 @@
 
 Console.Clear();
 
+int[] collection = CreateArray();
+FillArray(collection);
+Console.WriteLine(String.Join(", ", collection));
+int res = BinaryToDecimal(collection);
+Console.WriteLine(res);
+
+
+int[] CreateArray()
+{
+    int[] result = new int[new Random().Next(1, 7)];
+    return result;
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+int BinaryToDecimal(int[] array)
+{
+    int result = 0;
+    int count = 0;
+    for (int i = array.Length - 1; i > -1; i--)
+    {
+        result += array[i] * (int)Math.Pow(2, count);
+        count++;
+    }
+    return result;
+}
